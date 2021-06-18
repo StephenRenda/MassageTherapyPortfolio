28c7b7df7ceafcf6ea89533673f12713cc6830e0
Stephen Renda <stephenrenda@csus.edu>
2021-06-18 08:22:08 -0700

    Add project files.

---
+++ Pages/AboutMe.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Massage.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        private readonly ILogger<AboutMeModel> _logger;
+
+        public AboutMeModel(ILogger<AboutMeModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
